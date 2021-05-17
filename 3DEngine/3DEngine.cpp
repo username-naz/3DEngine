b8a509d8f4ad9f853d3d5368fd85d442c12766e1
username-naz <duttaaniruddha2001@gmail.com>
2021-05-17 23:48:46 +0530

    added illumination

--- 3DEngine/3DEngine.cpp
+++ 3DEngine/3DEngine.cpp
@@ -19,7 +19,6 @@ struct Mat4x4 {
 };
 
 
-
 class olcEngine3D : public olcConsoleGameEngine {
 private: 
     Mesh meshCube;
@@ -41,6 +40,40 @@ private:
             //o.z/= w;
         }
     }
+
+    CHAR_INFO GetColour(float lum)
+    {
+        short bg_col, fg_col;
+        wchar_t sym;
+        int pixel_bw = (int)(13.0f * lum);
+        switch (pixel_bw)
+        {
+        case 0: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID; break;
+
+        case 1: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_QUARTER; break;
+        case 2: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_HALF; break;
+        case 3: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_THREEQUARTERS; break;
+        case 4: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_SOLID; break;
+
+        case 5: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_QUARTER; break;
+        case 6: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_HALF; break;
+        case 7: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_THREEQUARTERS; break;
+        case 8: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_SOLID; break;
+
+        case 9:  bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_QUARTER; break;
+        case 10: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_HALF; break;
+        case 11: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_THREEQUARTERS; break;
+        case 12: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_SOLID; break;
+        default:
+            bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID;
+        }
+
+        CHAR_INFO c;
+        c.Attributes = bg_col | fg_col;
+        c.Char.UnicodeChar = sym;
+        return c;
+    }
+
 
 public:
     olcEngine3D(){
@@ -174,10 +207,20 @@ public:
                 light_direction.y /= magnitude; 
                 light_direction.z /= magnitude;
 
+                float dotProduct = normal.x * light_direction.x +
+                                   normal.y * light_direction.y +
+                                   normal.z * light_direction.z;
+                
+                CHAR_INFO c = GetColour(dotProduct);
+                triTranslated.col = c.Attributes;
+                triTranslated.sym = c.Char.UnicodeChar;
+
                 //3D to 2D
                 multiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
                 multiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
                 multiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+                triProjected.col = triTranslated.col;
+                triProjected.sym = triTranslated.sym;
 
                 //Scale into view
                 triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
@@ -195,7 +238,13 @@ public:
                 FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
                     triProjected.p[1].x, triProjected.p[1].y,
                     triProjected.p[2].x, triProjected.p[2].y,
-                    PIXEL_SOLID, FG_WHITE);
+                    triProjected.sym, triProjected.col);
+
+                  /*  DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                        triProjected.p[1].x, triProjected.p[1].y,
+                        triProjected.p[2].x, triProjected.p[2].y,
+                        PIXEL_SOLID, FG_BLACK);*/
+
             }
         }
 
